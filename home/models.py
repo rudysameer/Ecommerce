cdfd09cedf8424284c50bf4903646ce52c41604e
rudysameer <matinamaicha23@gmail.com>
2023-09-21 12:32:13 +0545

    Products displayed

--- home/models.py
+++ home/models.py
@@ -54,3 +54,22 @@ class SiteInfo(models.Model):
 
     def __str__(self):
         return self.address
+
+
+STOCK = (('In Stock','in Stock'),('Out of Stock','ut of Stock'))
+LABELS = (('default','default'),('hot','hot'),('new','New'),('sale','sale'))
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField(blank=True)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField(blank=True)
+    specification = models.TextField(blank=True)
+    slug = models.TextField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    stock = models.CharField(max_length=100, choices=STOCK)
+    labels = models.CharField(max_length=100,choices=LABELS)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View
-
+from .models import *
 # Create your views here.
 class BaseView(View):
     views = {}
+    views['categories'] = Category.objects.all()
+    views['brands'] = Brand.objects.all()
+    views['siteinfos'] = SiteInfo.objects.all()
 
 class HomeView(BaseView):
     def get(self,request):
-
-
-        return render(request,'index.html')
+        self.views
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['hots'] = Product.objects.filter(labels = 'hot')
+        self.views['news'] = Product.objects.filter(labels='new')
+        self.views['sales'] = Product.objects.filter(labels='sale')
+        return render(request,'index.html',self.views)
